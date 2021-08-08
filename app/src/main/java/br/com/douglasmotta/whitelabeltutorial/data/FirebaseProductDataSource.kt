04cfa1a5095eb7e50ba1e59b86f48185bac91b65
Eduardo Maxwell <eduardomaxwell2011@gmail.com>
2021-08-08 15:01:09 -0400

    update app kotlin

---
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/data/FirebaseProductDataSource.kt
@@ -0,0 +1,81 @@
+package br.com.douglasmotta.whitelabeltutorial.data
+
+import android.net.Uri
+import br.com.douglasmotta.whitelabeltutorial.BuildConfig
+import br.com.douglasmotta.whitelabeltutorial.domain.model.Product
+import br.com.douglasmotta.whitelabeltutorial.util.COLLECTION_PRODUCTS
+import br.com.douglasmotta.whitelabeltutorial.util.COLLECTION_ROOT
+import br.com.douglasmotta.whitelabeltutorial.util.STORAGE_IMAGES
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.coroutines.coroutineScope
+import java.util.*
+import javax.inject.Inject
+import kotlin.coroutines.suspendCoroutine
+
+class FirebaseProductDataSource @Inject constructor(
+    firebaseFirestore: FirebaseFirestore,
+    firebaseStorage: FirebaseStorage
+) : ProductDataSource {
+    
+    private val documentReference = firebaseFirestore
+        .document("$COLLECTION_ROOT/${BuildConfig.FIREBASE_FLAVOR_COLLECTION}/")
+
+    private val storageReference = firebaseStorage.reference
+
+
+    override suspend fun getProducts(): List<Product> {
+
+        return suspendCoroutine { continuation ->
+            val productsReference = documentReference.collection(COLLECTION_PRODUCTS)
+            productsReference.get().addOnSuccessListener { documents ->
+                val products = mutableListOf<Product>()
+                for (document in documents) {
+                    document.toObject(Product::class.java).run {
+                        products.add(this)
+                    }
+                }
+                continuation.resumeWith(Result.success(products))
+            }
+
+            productsReference.get().addOnFailureListener { ex ->
+                continuation.resumeWith(Result.failure(ex))
+            }
+        }
+    }
+
+    override suspend fun uploadProductImage(imageUri: Uri): String {
+        return suspendCoroutine { continuation ->
+            val randomKey = UUID.randomUUID()
+            val childReference = storageReference.child(
+                "$STORAGE_IMAGES/${BuildConfig.FIREBASE_FLAVOR_COLLECTION}/$randomKey"
+            )
+
+            childReference.putFile(imageUri)
+                .addOnSuccessListener { taskSnapshot ->
+                    taskSnapshot.storage.downloadUrl.addOnSuccessListener { uri ->
+                        val path = uri.toString()
+                        continuation.resumeWith(Result.success(path))
+                    }
+                }
+                .addOnFailureListener { ex ->
+                    continuation.resumeWith(Result.failure(ex))
+                }
+        }
+    }
+
+    override suspend fun createProduct(product: Product): Product {
+        return suspendCoroutine { continuation ->
+            documentReference
+                .collection(COLLECTION_PRODUCTS)
+                .document(System.currentTimeMillis().toString())
+                .set(product)
+                .addOnSuccessListener {
+                    continuation.resumeWith(Result.success(product))
+                }
+                .addOnFailureListener { ex ->
+                    continuation.resumeWith(Result.failure(ex))
+                }
+        }
+    }
+}
---
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/addproduct/AddProductFragment.kt
@@ -0,0 +1,87 @@
+package br.com.douglasmotta.whitelabeltutorial.ui.addproduct
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.viewModels
+import br.com.douglasmotta.whitelabeltutorial.databinding.AddProductFragmentBinding
+import br.com.douglasmotta.whitelabeltutorial.util.CurrencyTextWatcher
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.textfield.TextInputLayout
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AddProductFragment : BottomSheetDialogFragment() {
+
+    private var _binding: AddProductFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    private var imageUri: Uri? = null
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            imageUri = uri
+            binding.imageProduct.setImageURI(uri)
+        }
+
+    private val viewModel: AddProductViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = AddProductFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeMVEvents()
+        setListeners()
+    }
+
+    private fun observeMVEvents() {
+        viewModel.imageUriErrorResId.observe(viewLifecycleOwner) { drawableResId ->
+            binding.imageProduct.setBackgroundResource(drawableResId)
+        }
+
+        viewModel.descriptionFieldErrorResId.observe(viewLifecycleOwner) { stringResId ->
+            binding.inputLayoutDescription.setError(stringResId)
+        }
+
+        viewModel.priceFieldErrorResId.observe(viewLifecycleOwner) { stringResId ->
+            binding.inputLayoutPrice.setError(stringResId)
+        }
+    }
+
+    private fun TextInputLayout.setError(stringRedId: Int?) {
+        error = if (stringRedId != null) {
+            getString(stringRedId)
+        } else null
+    }
+
+    private fun setListeners() {
+        binding.imageProduct.setOnClickListener {
+            chooseImage()
+        }
+
+        binding.buttonAddProduct.setOnClickListener {
+            val description = binding.inputDescription.text.toString()
+            val price = binding.inputPrice.text.toString()
+            viewModel.createProduct(description, price, imageUri)
+        }
+
+        binding.inputPrice.run {
+            addTextChangedListener(CurrencyTextWatcher(this))
+        }
+    }
+
+    private fun chooseImage() {
+        getContent.launch("image/*")
+    }
+
+}
